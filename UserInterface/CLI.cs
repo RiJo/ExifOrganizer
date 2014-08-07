72b608633597bb2cff4882c39a7816ed5916e37a
RiJo <rajoo@mail.com>
2014-08-07 19:29:08 +0200

    Initial command line interface implementation

--- UserInterface/CLI.cs
+++ UserInterface/CLI.cs
@@ -16,10 +16,12 @@
 // this program. If not, see http://www.gnu.org/licenses/.
 //
 
+using ExifOrganizer.Organizer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ExifOrganizer.UI
@@ -33,9 +35,58 @@ namespace ExifOrganizer.UI
 
         public void Run(string[] args)
         {
-            System.Diagnostics.Debugger.Launch();
-            foreach (Arg arg in ParseArgs.Parse(args, new string[] { "-v" }, new string[] { "-s", "-d" }))
+            IEnumerable<Arg> parsedArgs = ParseArgs.Parse(args, new string[] { "-r" }, new string[] { "-s", "-d" });
+#if DEBUG
+            foreach (Arg arg in parsedArgs)
                 Console.WriteLine(String.Format(" * {0}", arg));
-        } 
+#endif
+
+
+            MediaOrganizer organizer = new MediaOrganizer();
+            organizer.DuplicateMode = DuplicateMode.KeepAll;
+            organizer.CopyMode = CopyMode.Delta;
+            //organizer.DestinationPatternImage = patternImage.Text;
+            //organizer.DestinationPatternVideo = patternVideo.Text;
+            //organizer.DestinationPatternAudio = patternAudio.Text;
+            organizer.Localization = Thread.CurrentThread.CurrentUICulture;
+
+            string source = null;
+            string destination = null;
+            bool recursive = false;
+            foreach (Arg arg in parsedArgs)
+            {
+                switch (arg.Key)
+                {
+                    case "-s":
+                        source = arg.Value;
+                        break;
+                    case "-d":
+                        destination = arg.Value;
+                        break;
+                    case "-r":
+                        recursive = true;
+                        break;
+                }
+            }
+            organizer.Recursive = true;
+
+            if (String.IsNullOrEmpty(source))
+                throw new ArgumentException("No source path given (-s)");
+            if (String.IsNullOrEmpty(destination))
+                throw new ArgumentException("No destination path given (-d)");
+
+            CopyItems items = organizer.Parse(source, destination);
+            try
+            {
+                organizer.Organize(items);
+                Console.WriteLine("Media organization completed successfully");
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                Environment.Exit(1);
+            }
+        }
     }
 }
