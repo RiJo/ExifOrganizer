234ba96c030008a99ed1646dfecee95e68fee0d1
RiJo <rajoo@mail.com>
2014-10-01 17:34:31 +0200

    Extracted CopyItem class to own file

---
+++ MediaOrganizer/CopyItem.cs
@@ -0,0 +1,38 @@
+﻿using ExifOrganizer.Meta;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExifOrganizer.Organizer
+{
+    public class CopyItem
+    {
+        public string sourcePath;
+        public string destinationPath;
+        public FileInfo sourceInfo;
+        public Dictionary<MetaKey, object> meta;
+
+        private string checksum;
+
+        public CopyItem()
+        {
+        }
+
+        public string GetChecksum()
+        {
+            if (!String.IsNullOrEmpty(checksum))
+                return checksum;
+
+            checksum = sourceInfo.GetMD5Sum();
+            return checksum;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("[{0}] ---> [{1}]", sourcePath, destinationPath);
+        }
+    }
+}
--- MediaOrganizer/CopyItems.cs
+++ MediaOrganizer/CopyItems.cs
@@ -26,34 +26,6 @@ using System.Threading.Tasks;
 
 namespace ExifOrganizer.Organizer
 {
-    public class CopyItem
-    {
-        public string sourcePath;
-        public string destinationPath;
-        public FileInfo sourceInfo;
-        public Dictionary<MetaKey, object> meta;
-
-        private string checksum;
-
-        public CopyItem()
-        {
-        }
-
-        public string GetChecksum()
-        {
-            if (!String.IsNullOrEmpty(checksum))
-                return checksum;
-
-            checksum = sourceInfo.GetMD5Sum();
-            return checksum;
-        }
-
-        public override string ToString()
-        {
-            return String.Format("[{0}] ---> [{1}]", sourcePath, destinationPath);
-        }
-    }
-
     public class CopyItems
     {
         public string sourcePath;
