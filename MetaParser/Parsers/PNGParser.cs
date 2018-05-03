644cba104c6697432b0b0099772c8031c41a2cea
RiJo <rajoo@mail.com>
2018-05-03 18:37:37 +0200

    Parse PNG IHDR image header to retrieve width and height

--- MetaParser/Parsers/PNGParser.cs
+++ MetaParser/Parsers/PNGParser.cs
@@ -32,7 +32,23 @@ namespace ExifOrganizer.Meta.Parsers
 
         private enum PNGTag
         {
+            ImageWidth,
+            ImageHeight,
+            BitDepth,
+            ColorType,
+            CompressionMethod,
+            FilterMethod,
+            InterlaceMethod,
             Comment
+        }
+
+        private enum PNGBitDepth : byte
+        {
+            Greyscale = 0,
+            Truecolor = 2,
+            IndexedColor = 3,
+            GreyscaleWithAlpha = 4,
+            TruecolorWithAlpha = 6
         }
 
         public static MetaData Parse(string filename)
@@ -52,6 +68,10 @@ namespace ExifOrganizer.Meta.Parsers
             MetaData meta = GetBaseMetaFileData(filename, MetaType.Image);
 
             Dictionary<PNGTag, object> png = ParsePNG(filename);
+            if (png.ContainsKey(PNGTag.ImageWidth))
+                meta.Data[MetaKey.Width] = png[PNGTag.ImageWidth];
+            if (png.ContainsKey(PNGTag.ImageHeight))
+                meta.Data[MetaKey.Height] = png[PNGTag.ImageHeight];
             if (png.ContainsKey(PNGTag.Comment))
                 meta.Data[MetaKey.Comment] = png[PNGTag.Comment];
 
@@ -76,20 +96,16 @@ namespace ExifOrganizer.Meta.Parsers
                     int chunkLength = BitConverter.ToInt32(chunkHeader.Take(4).Reverse().ToArray(), 0);
                     string chunkType = Encoding.ASCII.GetString(chunkHeader, 4, 4);
 
-                    KeyValuePair<string, string>? kvp = GetChunkData(stream, chunkLength, chunkType);
-                    if (!kvp.HasValue)
-                        continue;
-
-                    switch (kvp.Value.Key)
+                    foreach (var kvp in GetChunkData(stream, chunkLength, chunkType))
                     {
-                        case "Comment":
-                            tags[PNGTag.Comment] = kvp.Value.Value;
-                            break;
-
-                        default:
-                            Trace.WriteLine($"[PNGParser] ignoring meta data: \"{kvp.Value.Key}\" = \"{kvp.Value.Value}\"");
-                            break;
+                        if (tags.ContainsKey(kvp.Key))
+                        {
+                            Trace.WriteLine($"[PNGParser] duplicate meta key ignored: \"{kvp.Key}\"");
+                            continue;
+                        }
+                        tags[kvp.Key] = kvp.Value;
                     }
+
                 }
             }
 
@@ -114,34 +130,73 @@ namespace ExifOrganizer.Meta.Parsers
             return true;
         }
 
-        private static KeyValuePair<string, string>? GetChunkData(Stream stream, int length, string type)
+        private static Dictionary<PNGTag, object> GetChunkData(Stream stream, int length, string type)
         {
+            Dictionary<PNGTag, object> tags = new Dictionary<PNGTag, object>();
+
+            // TODO: parse "eXIf" - Exif meta data
+            // TODO: parse "pHYs" - indended pixel size, ratio, etc.
+            // TODO: parse "tIME" - store time for last change
+            // TODO: parse "zTXt" - compressed text
             switch (type)
             {
+                case "IHDR":
+                    {
+                        if (length != 13)
+                            throw new MetaParseException($"Invalid PNG \"IDHR\" chunk length: {length}");
+                        byte[] chunkData = new byte[length];
+                        if (stream.Read(chunkData, 0, chunkData.Length) != chunkData.Length)
+                            throw new MetaParseException($"Unable to read full PNG chunk \"{type}\" data");
+
+                        stream.Position += 4; // TODO: verify CRC32
+
+                        tags[PNGTag.ImageWidth] = BitConverter.ToInt32(chunkData.Take(4).Reverse().ToArray(), 0);
+                        tags[PNGTag.ImageHeight] = BitConverter.ToInt32(chunkData.Skip(4).Take(4).Reverse().ToArray(), 0);
+                        tags[PNGTag.BitDepth] = (int)chunkData[8];
+                        tags[PNGTag.ColorType] = (PNGBitDepth)chunkData[9];
+                        tags[PNGTag.CompressionMethod] = (int)chunkData[10];
+                        tags[PNGTag.FilterMethod] = (int)chunkData[11];
+                        tags[PNGTag.InterlaceMethod] = (int)chunkData[12];
+                    }
+                    break;
+
                 case "tEXt":
                 case "iTXt":
-                    byte[] chunkData = new byte[length];
-                    if (stream.Read(chunkData, 0, chunkData.Length) != chunkData.Length)
-                        throw new MetaParseException($"Unable to read full PNG chunk \"{type}\" data");
+                    {
+                        byte[] chunkData = new byte[length];
+                        if (stream.Read(chunkData, 0, chunkData.Length) != chunkData.Length)
+                            throw new MetaParseException($"Unable to read full PNG chunk \"{type}\" data");
 
-                    byte[] key = chunkData.TakeWhile(c => c != '\0').ToArray();
-                    if (key.Length == length)
-                        throw new MetaParseException($"Invalid PNG key-value data in chunk \"{type}\"");
-                    byte[] value = chunkData.Skip(key.Length + 1).ToArray();
+                        byte[] keyData = chunkData.TakeWhile(c => c != '\0').ToArray();
+                        if (keyData.Length == length)
+                            throw new MetaParseException($"Invalid PNG key-value data in chunk \"{type}\"");
+                        byte[] valueData = chunkData.Skip(keyData.Length + 1).ToArray();
 
-                    stream.Position += 4; // Skip CRC32
+                        stream.Position += 4; // TODO: verify CRC32
 
-                    if (type == "iTXt")
-                        return new KeyValuePair<string, string>(Encoding.ASCII.GetString(key), Encoding.UTF8.GetString(value));
-                    else
-                        return new KeyValuePair<string, string>(Encoding.ASCII.GetString(key), iso_8859_1.GetString(value));
+                        string key = Encoding.ASCII.GetString(keyData);
+                        string value = (type == "iTXt") ? Encoding.UTF8.GetString(valueData) : iso_8859_1.GetString(valueData);
+                        switch (key)
+                        {
+                            case "Comment":
+                                tags[PNGTag.Comment] = value;
+                                break;
+
+                            default:
+                                Trace.WriteLine($"[PNGParser] ignoring \"{type}\" chunk key: \"{key}\"");
+                                break;
+                        }
+                    }
+                    break;
 
                 default:
                     Trace.WriteLine($"[PNGParser] ignoring chunk type: \"{type}\"");
                     stream.Position += length; // Skip data
                     stream.Position += 4; // Skip CRC32
-                    return null;
+                    break;
             }
+
+            return tags;
         }
     }
 }
